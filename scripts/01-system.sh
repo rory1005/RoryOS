5c434410c921ed0876cac7bc9f1dd2034e248210
rory <ncll1986@gmail.com>
2026-07-01 08:05:33 +0800

    Refactor system setup script for default configuration

--- scripts/01-system.sh
+++ scripts/01-system.sh
@@ -7,12 +7,12 @@
 #     01-system.sh
 #
 # 文件作用：
-#     系统基础设置。
+#     系统基础配置模块。
 #
 # 当前功能：
 #     ① 设置默认主机名为 RoryOS
 #     ② 设置默认时区为 Asia/Shanghai
-#     ③ 设置默认 LAN IP 为 192.168.10.1
+#     ③ 使用 uci-defaults 首次开机自动配置
 #
 # 作者：
 #     Rory
@@ -31,44 +31,25 @@ set -e
 
 OPENWRT_DIR="${1:-openwrt}"
 
-echo "【系统】开始系统基础设置..."
+echo "【系统】开始配置系统默认设置..."
 
-mkdir -p "$OPENWRT_DIR/files/etc/config"
+mkdir -p "$OPENWRT_DIR/files/etc/uci-defaults"
 
-cat > "$OPENWRT_DIR/files/etc/config/system" <<'EOF'
-config system
-	option hostname 'RoryOS'
-	option timezone 'CST-8'
-	option zonename 'Asia/Shanghai'
-	option ttylogin '0'
-	option log_size '64'
-	option urandom_seed '0'
+cat > "$OPENWRT_DIR/files/etc/uci-defaults/97-roryos-system" <<'EOF'
+#!/bin/sh
+
+# RoryOS 系统默认配置
+# 首次开机自动执行
+
+uci set system.@system[0].hostname='RoryOS'
+uci set system.@system[0].timezone='CST-8'
+uci set system.@system[0].zonename='Asia/Shanghai'
+
+uci commit system
+
+exit 0
 EOF
 
-cat > "$OPENWRT_DIR/files/etc/config/network" <<'EOF'
-config interface 'loopback'
-	option device 'lo'
-	option proto 'static'
-	option ipaddr '127.0.0.1'
-	option netmask '255.0.0.0'
+chmod +x "$OPENWRT_DIR/files/etc/uci-defaults/97-roryos-system"
 
-config globals 'globals'
-	option ula_prefix 'auto'
-
-config device
-	option name 'br-lan'
-	option type 'bridge'
-	list ports 'lan1'
-	list ports 'lan2'
-	list ports 'lan3'
-	list ports 'wan'
-
-config interface 'lan'
-	option device 'br-lan'
-	option proto 'static'
-	option ipaddr '192.168.10.1'
-	option netmask '255.255.255.0'
-	option ip6assign '60'
-EOF
-
-echo "【系统】系统基础设置完成。"
+echo "【系统】系统默认设置完成。"
